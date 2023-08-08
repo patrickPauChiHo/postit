9a5849214586402d55b91ec202216f98bb4a7948
PatrickPau <patrickpauchiho@gmail.com>
2023-08-08 14:41:43 +1000

    add type to post component

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -3,7 +3,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Post({ avatar, name, postTitle, id, comments }) {
+type PostProps = {
+  avatar: string;
+  name: string;
+  postTitle: string;
+  id: string;
+  comments: string[];
+};
+
+export default function Post({
+  avatar,
+  name,
+  postTitle,
+  id,
+  comments,
+}: PostProps) {
   return (
     <div className="bg-white my-8 p-8 rounded-lg">
       <div className="flex items-center gap-2">
